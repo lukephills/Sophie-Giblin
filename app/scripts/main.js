e75e90be18c14065fa3edc7a3f853a28b42d501d
Luke Phillips <luke@femurdesign.com>
2014-08-30 14:20:37 +0100

    Finished first draft

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
-console.log('\'Allo \'Allo!');
+// var lineL1 = document.getElementById('lineL1');
+// var lineR1 = document.getElementById('lineR1');
+
+
+$('.title-text').waypoint(function() {
+  $(this).removeClass('hidden');
+}, {offset: '90%'});
+
+$('.line').waypoint(function() {
+  $(this).addClass('full');
+}, {offset: '90%'});
+
+
